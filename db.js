756eb1b05968e05bb68f019f6081e0e8715b6335
Neha <nehasijaria@gmail.com>
2022-01-13 10:46:06 -0500

    nodemon/update script/

--- db.js
+++ db.js
@@ -3,6 +3,6 @@
 let players = [
   { _id: 1, name: "Sachin", runs: "18426" },
   { _id: 2, name: "Dhoni", runs: "10500" },
-  { _id: 3, name: "Virat", runs: "10843" }
+  { _id: 3, name: "ViratS", runs: "10843" }
 ]
 module.exports = players;
