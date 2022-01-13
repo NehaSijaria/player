7b66c36abc2f35b763f598bdc9f101e1394b880b
Neha <nehasijaria@gmail.com>
2022-01-13 10:18:33 -0500

    create route to get playerslist/indiv player

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
-// dummyDatabase.js
+//dummy databse
 
 let players = [
   { _id: 1, name: "Sachin", runs: "18426" },
   { _id: 2, name: "Dhoni", runs: "10500" },
   { _id: 3, name: "Virat", runs: "10843" }
-];
-
+]
 module.exports = players;
